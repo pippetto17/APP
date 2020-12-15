631861d7e9ec83ebf77a4c841a48720bb4b80cf8
pippetto17 <73800225+pippetto17@users.noreply.github.com>
2020-12-15 12:00:12 +0100

    provamo

---
+++ APP/app/src/main/java/com/example/app/create_match_soccer.java
@@ -0,0 +1,37 @@
+package com.example.app;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class create_match_soccer extends AppCompatActivity {
+    private TextView textName;
+    private TextView textSurname;
+    private TextView textPhone;
+    private TextView textUser;
+    private TextView textPass;
+    private TextView textConfirmPass;
+    private Button reg_back;
+
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+        textName = findViewById(R.id.textName);
+        textSurname = findViewById(R.id.textSurname);
+        textPhone = findViewById(R.id.textPhone);
+        textUser = findViewById(R.id.textUser);
+        textPass = findViewById(R.id.textPass);
+        textConfirmPass = findViewById(R.id.textConfirmPass);
+        reg_back = findViewById(R.id.reg_back);
+
+
+
+    }
+}
+
+
