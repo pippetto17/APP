5a6d4cf4a5cca6b16824e6305c9eb0364cb426cb
pippetto17 <73800225+pippetto17@users.noreply.github.com>
2020-12-22 23:03:12 +0100

    sesese

---
+++ APP/app/src/main/java/com/example/app/FirstPageActivity.java
@@ -0,0 +1,19 @@
+package com.example.app;
+
+import android.app.DatePickerDialog;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Calendar;
+
+public class FirstPageActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_page);
+    }
+}
