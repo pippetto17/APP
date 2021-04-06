29510cae1b428700ece0c77db40d08e935fce041
SKEGGIA27 <75479919+SKEGGIA27@users.noreply.github.com>
2021-04-06 03:25:32 +0200

    Implementate Shared Prefrence

--- APP/app/src/main/java/com/example/app/Login.java
+++ APP/app/src/main/java/com/example/app/Login.java
@@ -1,26 +1,20 @@
 package com.example.app;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.util.Patterns;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.textfield.TextInputEditText;
 import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class Login extends AppCompatActivity {
@@ -29,6 +23,9 @@ public class Login extends AppCompatActivity {
     EditText EditTextEmail, EditTextPassword;
     CardView buttonLogin;
     TextView textViewSignup;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,45 +50,59 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String email, password;
-                email = String.valueOf(EditTextEmail.getText());
-                password = String.valueOf(EditTextPassword.getText());
-
-
-                if (!email.isEmpty() && !password.isEmpty()) {
-                    Handler handler = new Handler(Looper.getMainLooper());
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-
-                            String[] field = new String[2];
-                            field[0] = "email";
-                            field[1] = "password";
-
-                            String[] data = new String[2];
-                            data[0] = email;
-                            data[1] = password;
-                            PutData putData = new PutData("http://93.43.208.27/carletti/sportydb/login.php", "POST", field, data);
-
-                            if (putData.startPut()) {
-                                if (putData.onComplete()) {
-                                    String result = putData.getResult();
-                                    if (result.equals("Login riuscito")) {
-                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent(getApplicationContext(), Search.class);
-                                        startActivity(intent);
-                                        finish();
-                                    } else {
-                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            }
-                        }
-                    });
-                } else {
-                    Toast.makeText(getApplicationContext(), "Sono richiesti tutti i campi", Toast.LENGTH_SHORT).show();
-                }
+                //Vengono estrapolati mail e psw dalle textView e viene chiamato il metodo di Login
+                String email = String.valueOf(EditTextEmail.getText());
+                String password = String.valueOf(EditTextPassword.getText());
+                userLogin(email, password);
             }
         });
+    }
+
+
+    //Effettua il login e scrive le shared preferences
+    public void userLogin(String email, String password){
+
+        if (!email.isEmpty() && !password.isEmpty()) {
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+
+                    String[] field = new String[2];
+                    field[0] = "email";
+                    field[1] = "password";
+
+                    String[] data = new String[2];
+                    data[0] = email;
+                    data[1] = password;
+                    PutData putData = new PutData("http://93.43.208.27/carletti/sportydb/login.php", "POST", field, data);
+
+                    if (putData.startPut()) {
+                        if (putData.onComplete()) {
+                            String result = putData.getResult();
+                            if (result.equals("Login riuscito")) {
+                                Toast.makeText(Login.this, result , Toast.LENGTH_LONG).show();
+
+
+                                //Scrive le shared prefrences
+                                sharedPreferences = getSharedPreferences("MySharedPreferences", Context.MODE_PRIVATE);
+                                editor = sharedPreferences.edit();
+                                editor.putString("emailLogin", email);
+                                editor.putString("passwordLogin", password);
+                                editor.commit();
+
+                                Intent intent = new Intent (Login.this, Search.class);
+                                startActivity(intent);
+
+                            } else {
+                                Toast.makeText(Login.this, result, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(Login.this, "Sono richiesti tutti i campi", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- APP/app/src/main/java/com/example/app/MainActivity.java
+++ APP/app/src/main/java/com/example/app/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.app;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Legge le shared preferences dal file di testo
+        sharedPreferences  = getSharedPreferences("MySharedPreferences", Context.MODE_PRIVATE);
+        String email = sharedPreferences.getString("emailLogin", "");
+        String password = sharedPreferences.getString("passwordLogin", "");
+
+        if(!email.isEmpty() && !password.isEmpty())
+        {
+            //Viene effettuato il login automatico se dovessero essere presenti le credenziali
+            userLogin(email, password);
+        }
+        else {
+
+            //Apre la schermata di login
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    public void userLogin(String email, String password){
+
+        if (!email.isEmpty() && !password.isEmpty()) {
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+
+                    String[] field = new String[2];
+                    field[0] = "email";
+                    field[1] = "password";
+
+                    String[] data = new String[2];
+                    data[0] = email;
+                    data[1] = password;
+                    PutData putData = new PutData("http://93.43.208.27/carletti/sportydb/login.php", "POST", field, data);
+
+                    if (putData.startPut()) {
+                        if (putData.onComplete()) {
+                            String result = putData.getResult();
+                            if (result.equals("Login riuscito")) {
+                                Toast.makeText(MainActivity.this, "Bentornato" , Toast.LENGTH_LONG).show();
+
+                                Intent intent = new Intent (MainActivity.this, Search.class);
+                                startActivity(intent);
+
+                            } else {
+                                Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(getApplicationContext(), "Sono richiesti tutti i campi", Toast.LENGTH_SHORT).show();
+        }
     }
 }
